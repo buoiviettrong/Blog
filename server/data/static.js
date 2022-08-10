ef3844d69bb79eb4c176db327f8a97ec2747eb7c
N.D.Nghĩa <83491342+buoiviettrong@users.noreply.github.com>
2022-08-10 10:12:05 +0700

    data static test

---
+++ server/data/static.js
@@ -0,0 +1,36 @@
+const posts = [{
+        id: 1,
+        title: "Post 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: 2,
+        title: "Post 2",
+        description: "Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    }
+]
+
+const users = [{
+        id: 1,
+        fullName: "John Doe",
+        avatar: "http://www.example.com/avatar/john",
+        email: "john.doe@example.com",
+    },
+    {
+        id: 2,
+        fullName: "Jane Doe",
+        avatar: "http://www.example.com/avatar/jane",
+        email: "jane.doe@example.com",
+    },
+    {
+        id: 3,
+        fullName: "Doe Smith",
+        avatar: "http://www.example.com/avatar/doe",
+        email: "doe.smith@example.com",
+    }
+]
+
+module.exports = {
+    posts,
+    users
+}
--- server/resolver/resolver.js
+++ server/resolver/resolver.js
@@ -1,37 +1,11 @@
+const { posts, users } = require('../data/static')
+
 // Ham tra du lieu that su 
 
 const resolvers = {
     Query: {
-        posts: () => [{
-                id: 1,
-                title: "Post 1",
-                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-            },
-            {
-                id: 2,
-                title: "Post 2",
-                content: "Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-            }
-        ],
-        users: () => [{
-                id: 1,
-                fullName: "John Doe",
-                avatar: "http://www.example.com/avatar/john",
-                email: "john.doe@example.com",
-            },
-            {
-                id: 2,
-                fullName: "Jane Doe",
-                avatar: "http://www.example.com/avatar/jane",
-                email: "jane.doe@example.com",
-            },
-            {
-                id: 3,
-                fullName: "Doe Smith",
-                avatar: "http://www.example.com/avatar/doe",
-                email: "doe.smith@example.com",
-            }
-        ]
+        posts: () => posts,
+        users: () => users,
     }
 }
 
